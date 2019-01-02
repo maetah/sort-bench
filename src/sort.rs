e4fdfc200cec89196ece88c3a78cc52a2f97423c
maetah <maetah@pm.me>
2019-01-02 18:37:04 +0800

    Quicksort

--- src/sort.rs
+++ src/sort.rs
@@ -1,4 +1,4 @@
-pub fn bubble<T : Ord>(xs : &mut Vec<T>) {
+pub fn bubble<T : Ord>(xs : &mut [T]) {
     for i in 0 .. xs.len() {
         for j in 0 .. xs.len() {
             if xs[i] < xs[j] {
@@ -8,7 +8,7 @@ pub fn bubble<T : Ord>(xs : &mut Vec<T>) {
     }
 }
 
-pub fn insertion<T : Ord>(xs : &mut Vec<T>) {
+pub fn insertion<T : Ord>(xs : &mut [T]) {
     for i in 0 .. xs.len() - 1 {
         for j in (0 .. i + 1).rev() {
             if xs[j] > xs[j + 1] {
@@ -17,5 +17,32 @@ pub fn insertion<T : Ord>(xs : &mut Vec<T>) {
                 break;
             }
         }
+    }
+}
+
+pub fn quick<T : Ord>(xs : &mut [T]) {
+    fn partition<T : Ord>(ys : &mut [T]) -> usize {
+        let len = ys.len();
+        let last = len - 1;
+
+        ys.swap(len / 2, last);
+
+        let mut i = 0;
+        for j in 0 .. last {
+            if ys[j] < ys[last] {
+                ys.swap(i, j);
+                i += 1;
+            }
+        }
+
+        ys.swap(i, last);
+        i
+    }
+
+    let len = xs.len();
+    if len >= 2 {
+        let p = partition(xs);
+        quick(&mut xs[0 .. p]);
+        quick(&mut xs[p + 1 .. len]);
     }
 }
